61c3c6256d9291a0689542087e41fbcfba903119
Jefferson Aguiar <jfdruida@gmail.com>
2016-05-05 12:14:38 +0100

    Added files via upload

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats! the world is doomed!")
+
+if people > cats:
+    print("Not too many cats! the world is saved!")
+
+if people < dogs:
+    print("too many dogs! the world is doomed again!")
+
+if people > dogs:
+    print("Not too many dogs!")
+
+dogs += 5
+
+if people >= dogs:
+    print("people are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("people are less than or equal to dogs.")
+
+if people == dogs:
+    print("people are dogs.")
+
+
+
