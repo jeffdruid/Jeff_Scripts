6b69945fc35a48fd0c1b7e2a186f13dfe3a28f30
Jefferson Aguiar <jfdruida@gmail.com>
2016-05-05 12:03:48 +0100

    Added files via upload

---
+++ ex28.py
@@ -0,0 +1,21 @@
+True and True #true
+False and True #false
+1 == 1 and 2 == 1 #false
+'test' == 'test' #true
+1==1 or 2 != 1 # true
+True and 1 == 1 #True
+False and 0 != 0 #False
+True or 1 == 1 #True
+'test' == 'testing' #False
+1 != 0 and 2 == 1 #False
+'test' != 'testing' #True
+'test' == 1 #false
+not (True and False) # True
+not (1 == 1 and 0 != 1) #False
+not (10 == 1 or 1000 == 1000) #false
+not (1 != 10 or 3 == 4) #false
+not ('testing' == 'testing' and 'zed' == 'cool')#true
+1 == 1 and not ('testing ' == 1 or 1 == 0) #true
+'chunky' == 'bacon' and not (3 == 4 or 3 == 3) #false
+3 == 3 and not ('testing' == 'testing' or 'python' == 'fun')#false
+
